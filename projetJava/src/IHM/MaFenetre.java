5ec320d6da8680ba11c726bdaa765e2398c73e01
mattmax1 <matthieukoebsch@hotmail.com>
2020-12-16 17:55:41 +0100

    GBC

--- projetJava/src/IHM/MaFenetre.java
+++ projetJava/src/IHM/MaFenetre.java
@@ -12,7 +12,7 @@ public class MaFenetre extends JFrame {
     public MaFenetre(){
         super("Biblio.exe");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(650,350);
+        setSize(620,400);
         JPanel panel = new JPanel();
         this.setContentPane(panel);
         JMenuBar menuBar = new JMenuBar();
@@ -20,6 +20,11 @@ public class MaFenetre extends JFrame {
         JMenu fileMenu = new JMenu();
         fileMenu.setText("File");
         menuBar.add(fileMenu);
+
+        GridBagLayout layout = new GridBagLayout();
+        panel.setLayout(layout);
+
+        GridBagConstraints gbc = new GridBagConstraints();
 
         JFileChooser test = new JFileChooser();
         JMenuItem fichier = new JMenuItem("Ouvrir...");
@@ -31,13 +36,11 @@ public class MaFenetre extends JFrame {
                 test.setFileFilter(filter);
                 int retour = test.showOpenDialog(panel);
                 test.setVisible(true);
-<<<<<<< HEAD
-=======
                 File fText = test.getSelectedFile();
                 fText = fText.getAbsoluteFile();
                 System.out.println(fText);
 
->>>>>>> main
+
 
             }
         });
@@ -59,16 +62,29 @@ public class MaFenetre extends JFrame {
 
         JTable montableau = new JTable(donnees,entetes);
 
-        getContentPane().add(montableau.getTableHeader());
-        getContentPane().add(montableau);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 12;
+        gbc.gridheight = 1;
+
+
+        getContentPane().add(montableau.getTableHeader(),gbc);
+
 
         JButton buttonPlus = new JButton("Add");
-        panel.add(buttonPlus);
+        gbc.gridx = 5;
+        gbc.gridy = 7;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        panel.add(buttonPlus,gbc);
         JButton buttonSup = new JButton("Sup");
-        panel.add(buttonSup);
+        gbc.gridx = 6;
+        gbc.gridy = 7;
+        panel.add(buttonSup,gbc);
 
         JButton buttonValider = new JButton("Valider");
-        panel.add(buttonValider);
+
 
         JLabel label1 = new JLabel("Titre");
         JLabel label2 = new JLabel("Auteur");
@@ -91,20 +107,50 @@ public class MaFenetre extends JFrame {
         textArea.setPreferredSize(new Dimension(100,150));
 
 
-        panel.add(label1);
-        panel.add(textField1);
-        panel.add(label2);
-        panel.add(textField2);
-        panel.add(label3);
-        panel.add(textField3);
-        panel.add(label4);
-        panel.add(textField4);
-        panel.add(label5);
-        panel.add(textField5);
-        panel.add(label6);
-        panel.add(textArea);
+        gbc.gridx = 13;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        panel.add(label1,gbc);
+        gbc.gridy = 1;
+        panel.add(label2,gbc);
+        gbc.gridy = 2;
+        panel.add(label3,gbc);
+        gbc.gridy = 3;
+        panel.add(label4,gbc);
+        gbc.gridy = 4;
+        panel.add(label5,gbc);
 
-        panel.add(buttonValider);
+
+        gbc.gridx = 14;
+        gbc.gridy = 0;
+        panel.add(textField1,gbc);
+        gbc.gridy = 1;
+        panel.add(textField2,gbc);
+        gbc.gridy = 2;
+        panel.add(textField3,gbc);
+        gbc.gridy = 3;
+        panel.add(textField4,gbc);
+        gbc.gridy = 4;
+        panel.add(textField5,gbc);
+        gbc.gridy = 5;
+        panel.add(textArea,gbc);
+        gbc.gridy = 6;
+        panel.add(buttonValider,gbc);
+
+        gbc.gridx = 13;
+        gbc.gridy = 5;
+        gbc.insets = new Insets(0,0,130,0);
+        panel.add(label6,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 12;
+        gbc.gridheight = 6;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        getContentPane().add(montableau,gbc);
+
+
 
     }
 }
